a9334b708625fe16a6f4ef1314b68822ab2d278f
NunjiSwoo <renanoliveira30@hotmail.com>
2024-03-20 22:10:28 -0300

    feat(serializers.py): Criação de novas funções no repositories

--- weather/repositories.py
+++ weather/repositories.py
@@ -2,17 +2,16 @@ from django.conf import settings
 import pymongo
 
 class WeatherRepository:
-
-    collection = ''
-
-    def __init__(self, collectionName) -> None:
-        self.collection = collectionName
+    def __init__(self, collection_name) -> None:
+        self.collection = collection_name
 
     def getConnection(self):
         client = pymongo.MongoClient(
-            getattr(settings, "MONGO_CONNECTION_STRING"))
+            getattr(settings, "MONGO_CONNECTION_STRING")
+        )
         connection = client[
-            getattr(settings, "MONGO_DATABASE_NAME")]
+            getattr(settings, "MONGO_DATABASE_NAME")
+        ]
         return connection
     
     def getCollection(self):
@@ -20,9 +19,23 @@ class WeatherRepository:
         collection = conn[self.collection]
         return collection
     
+    def getByAttribute(self, attribute, value):
+        document = self.getCollection().find_one({f"{attribute}": value})
+
+    def delete(self, document) -> None:
+        self.getCollection().delete_one(document)
+		
+    def deleteAll(self) -> None:
+        self.getCollection().delete_many({})
+
+    def findOneById(self, id):
+        document = self.getCollection().find_one({"_id": id})
+        return document
+    
     def getAll(self):
         document = self.getCollection().find({})
         return document
     
     def insert(self, document):
-        self.getCollection().insert_one(document)
+        document = self.getCollection().insert_one(document)
+        return document
